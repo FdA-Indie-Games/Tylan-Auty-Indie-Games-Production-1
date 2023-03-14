2195086ca0fa0c9d8668b93589f862672337cc41
aut20000716 <115559792+aut20000716@users.noreply.github.com>
2023-03-14 11:55:00 +0000

    Update 1403 2
    Fixed automated movement to allow it to stop at end of pathway, successfully added movement back to start

--- Indie Games Production Unity Project/Assets/Scripts/Movement.cs
+++ Indie Games Production Unity Project/Assets/Scripts/Movement.cs
@@ -7,28 +7,56 @@ public class Movement : MonoBehaviour
 
     public float Speed;
     public bool SpeedStop;
-    public GameObject Endcheck;
+    //public GameObject Endcheck;
+    public float Position;
+    float EndPoint;
+    float RightEnd = 22.5f;
+    float LeftEnd = -22.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpeedStop = false;
+        EndPoint = RightEnd;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(SpeedStop == false)
+        if(SpeedStop == false & EndPoint == RightEnd)
         {
             transform.Translate(0,0,1 * Speed * Time.deltaTime);
+        }
+        if(SpeedStop == false & EndPoint == LeftEnd)
+        {
+             transform.Translate(0,0,-1 * Speed * Time.deltaTime);
         }
         if(SpeedStop == true)
         {
             transform.Translate(0,0,0 * Speed * Time.deltaTime);
         }
+
+        Position = gameObject.transform.position.x;
+
+        if (RightEnd == EndPoint & Position >= RightEnd)
+        {
+            SpeedStop = true;
+            StartCoroutine(DirectionChange());
+        }
+        if (LeftEnd == EndPoint & Position <= LeftEnd)
+        {
+            SpeedStop = true;
+        }
     }
 
-    public void OnTriggerEnter(Collider Endcheck)
+    IEnumerator DirectionChange()
+    {
+        yield return new WaitForSeconds(2);
+        SpeedStop = false;
+        EndPoint = LeftEnd;
+    }
+
+    /* public void OnTriggerEnter(Collider Endcheck)
     {
         SpeedStop = true;
-    }
+    }*/
 }
